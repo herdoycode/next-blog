7d538e8dbf2f28d429c85a270546b4f538c2d39c
= <=>
2023-11-28 01:03:12 +0600

    Create all required pages

--- app/components/PostCard.tsx
+++ app/components/PostCard.tsx
@@ -1,34 +1,29 @@
 import {
-  Inset,
-  Text,
-  Strong,
-  Card,
-  Box,
-  Flex,
-  Heading,
-  Button,
-} from "@radix-ui/themes";
-import {
   CalendarIcon,
   ChatBubbleIcon,
   DashboardIcon,
 } from "@radix-ui/react-icons";
-import React from "react";
+import {
+  Box,
+  Button,
+  Card,
+  Flex,
+  Heading,
+  Inset,
+  Text,
+} from "@radix-ui/themes";
+import Image from "next/image";
 
 const PostCard = () => {
   return (
     <Card className="hover:shadow-xl transition-shadow">
       <Inset clip="padding-box" side="top" pb="current">
-        <img
+        <Image
+          width={500}
+          height={100}
           src="https://images.unsplash.com/photo-1617050318658-a9a3175e34cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=80"
           alt="Bold typography"
-          style={{
-            display: "block",
-            objectFit: "cover",
-            width: "100%",
-            height: 200,
-            backgroundColor: "var(--gray-5)",
-          }}
+          style={{ width: "100%", height: "200px", objectFit: "cover" }}
         />
       </Inset>
       <Box py="3">
---
+++ app/contact/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <main>
+      <h2>Contact Page</h2>
+    </main>
+  );
+};
+
+export default Contact;
